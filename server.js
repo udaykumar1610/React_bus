db949ed3c5784524b1bf69417e3e7e4df917aa40
uday kumar <udaykumar92937@gmail.com>
2023-12-12 20:46:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+
+const usersRoute = require("./routes/usersRoute");
+ const busesRoute = require("./routes/busesRoute");
+ const bookingsRoute = require("./routes/bookingsRoute");
+
+ 
+ app.use("/api/bookings", bookingsRoute);
+
+
+app.use("/api/users", usersRoute);
+app.use("/api/buses", busesRoute);
+
+
+app.listen(port, () => console.log(`Node server listening on port ${port}!`));
+
+
